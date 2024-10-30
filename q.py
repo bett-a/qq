e3f1ac8ee781499475d065abb4a98f5a8f5569c5
Лаврова Елизавета Александровна <e.lavrova2@main.nsu.ru>
2024-10-30 14:14:25 +0700

    not working yet

--- q.py
+++ q.py
@@ -6,3 +6,21 @@ def insertion_sort(a):
             a[j + 1] = a[j]
             j -= 1
         a[j + 1] = tmp
+    return a
+
+def bubble_sort(a):
+    for i in range(len(a) - 1):
+        tmp = a[i]
+        j = i + 1
+        if tmp > a[j]:
+            a[i], a[j] = a[j], a[i]
+
+        return a
+
+def selection_sort(a):
+    for i in range(len(a)):
+        lst = a[i:]
+        minn = min(lst)
+        index_min = a.index(minn)
+        a[i], a[index_min] = a[index_min], a[i]
+    return a
